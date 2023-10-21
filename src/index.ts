c95a2dddc0528141d1336149f541c7146d05226b
igsr5 <igsr5.dev@gmail.com>
2023-10-22 05:28:44 +0900

    Add Express server with ping route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import express from "express";
+
+const app = express();
+const port = process.env.APP_PORT || 3000;
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server running at http://localhost:${port}/`);
+});
